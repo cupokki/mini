3f0891bac283773c53c87535f6297498940fcc1f
cupokki <nd1346@gmail.com>
2025-01-14 12:54:46 +0900

    add controller

--- src/main/java/io/goorm/mini/controller/testController.java
+++ src/main/java/io/goorm/mini/controller/testController.java
@@ -1,17 +1,14 @@
 package io.goorm.mini.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testController {
 
     @GetMapping
     public String test(){
-        return "Hello?";
-    }
-    @GetMapping("/hi")
-    public String hi(){
-        return "hi";
+        return "index";
     }
 }
